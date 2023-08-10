1820a443e5f317d8dd2d3f9d1d477a2575a79edb
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-08-09 21:28:06 -0300

    Spring

---
+++ lembretes/src/main/java/com/atividade/lembretes/LembretesApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.lembretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembretesApplication.class, args);
+	}
+
+}
---
+++ lembretes/src/test/java/com/atividade/lembretes/LembretesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.lembretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
